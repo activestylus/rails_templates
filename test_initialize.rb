aebb0403eaded4ef37154d86e740a48de6b98875
Steven Garcia <stevendgarcia@gmail.com>
2010-10-10 22:29:24 +0200

    test initializer

---
+++ test_initialize.rb
@@ -0,0 +1,23 @@
+#----------------------------------------------------------------------------
+# Setup RVM
+#----------------------------------------------------------------------------
+which_ruby = ask("Which rvm Ruby do you want to use?\r\n\r\n=>")
+puts "=" * 80
+puts "Setup RVM"
+puts "=" * 80
+run "rvm use #{which_ruby}"
+run "rvm gemset create #{app_name}"
+create_file ".rvmrc", <<-RVM
+rvm use #{which_ruby}@#{app_name}
+RVM
+
+#----------------------------------------------------------------------------
+# Bundle Gems
+#----------------------------------------------------------------------------
+puts "=" * 80
+puts "Bundle Gems"
+puts "=" * 80
+run "rvm use #{which_ruby}@#{app_name}"
+run "rvmsudo gem install rake"
+run "rvmsudo gem install bundler"
+run "bundle install"
