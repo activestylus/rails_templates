460aca8fe2e24885ca68d8766ffed2ba96f256b7
Steven Garcia <stevendgarcia@gmail.com>
2010-10-21 23:14:27 +0200

    Cleanup

--- install.rb
+++ install.rb
@@ -51,6 +51,17 @@ end
 apply  "#{root}actions/setup_environment_gems.rb"
 
 #----------------------------------------------------------------------------
+# Layout
+#----------------------------------------------------------------------------
+puts "=" * 80
+puts "Application Layout"
+puts "=" * 80
+inside "app/views/layouts" do
+  remove_file "application.html.erb"
+  get "#{root}files/layout.html.haml", "application.html.haml"
+end
+
+#----------------------------------------------------------------------------
 # Setup Git
 #----------------------------------------------------------------------------
 puts "=" * 80
