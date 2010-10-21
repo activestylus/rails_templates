e44424fb9627b0d6f49bbaa599e5eaca5d50d28e
Steven Garcia <stevendgarcia@gmail.com>
2010-10-22 00:23:14 +0200

    Fix devise task

--- install.rb
+++ install.rb
@@ -38,7 +38,7 @@ if yes?("Do you want to install Devise for authentication? (y/n)\r\n=> ")
   gem 'devise', :git => 'git://github.com/plataformatec/devise.git'
   gem 'warden'
   inside "config/initializers" do
-    get "#{git_dir}files/devise.rb", "devise.rb"
+    get "#{root}files/devise.rb", "devise.rb"
     gsub_file 'devise.rb', /# config.mailer_sender/ do
       "config.mailer_sender = '#{ask("What email address will this app send mail from?\r\n\r\n=>")}'"
     end
