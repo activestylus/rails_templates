dca55079a8909970cd3c5f0bbd5f34a7a11cd23d
Steven Garcia <stevendgarcia@gmail.com>
2011-01-07 20:38:48 +0100

    Add more UI modules

--- actions/install_rightjs.rb
+++ actions/install_rightjs.rb
@@ -1,6 +1,6 @@
 root = "http://github.com/activestylus/sass_tools/raw/master/"
-chosen_widgets = ask("Which RightJS Widgets would you like to use? (answer with array [1,4,5]  \"all\" or press Enter to skip)\r\n\r\n1. Autocompleter\r\n2. Calendar\r\n3. Colorpicker\r\n4.Dialog\r\n5. In Place Editor\r\n6. Lightbox\r\n7. Rater\r\n8. Resizable\r\n9. Selectable\r\n10. Slider\r\n11. Tabs\r\n12. Tooltips\r\n13.Uploader\r\n\r\n=>")
-right_widgets = %w(autocompleter calendar colorpicker in-edit lightbox rater resizable selectable slider tabs tooltips uploader)
+chosen_widgets = ask("Which RightJS Widgets would you like to use? (answer with array [1,4,5]  \"all\" or press Enter to skip)\r\n\r\n1. Autocompleter\r\n2. Billboard\r\n3. Calendar\r\n4. Colorpicker\r\n5.Dialog\r\n6. In Place Editor\r\n7. Lightbox\r\n8. Rater\r\n9. Resizable\r\n10. Selectable\r\n11. Slider\r\n12. Sortable\r\n13. Tabs\r\n14. Tooltips\r\n15.Uploader\r\n\r\n=>")
+right_widgets = %w(autocompleter billboard calendar colorpicker dialog in-edit lightbox rater resizable selectable slider sortable tabs tooltips uploader)
 
 #----------------------------------------------------------------------------
 # Javascript (RightJS)
